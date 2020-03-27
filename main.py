3d9fd92b189453596b335fca17aa86b9b982c8fd
Brian Chao <whereisbrianchao520@gmail.com>
2020-03-28 01:01:19 +0800

    add array filling functions

--- main.py
+++ main.py
@@ -14,16 +14,13 @@ def get_labeled_exif(exif):
         labeled[TAGS.get(key)] = val
     return labeled
 
-def read_image(file_name, image_dir = IMAGE_DIR):
+def read_image(file_name, image_dir = IMAGE_DIR, resize = False):
     file_path = os.path.join(image_dir, file_name)
-    print(file_path)
+    print("Reading image {} ...".format(file_path))
     img = Image.open(file_path)
     exifs = get_labeled_exif(img._getexif())
     exposure_sec, exposure_base = exifs['ExposureTime']
-    return {
-        'img': np.asarray(img), 
-        'exposure': exposure_sec * 1.0 / exposure_base
-    }
+    return np.asarray(img), exposure_sec * 1.0 / exposure_base
 
 def get_rgb_channels(image):
     ''' Image dimension is (H, W, C), where C is in RGB order '''
@@ -39,9 +36,34 @@ def sample_pixels(h, w):
             pos.append((i * h_step, j * w_step))
     return pos
         
-images = list(map(read_image, image_files))
-image_height = images[0]['img'].shape[0]
-image_width = images[0]['img'].shape[1]
+def z_weights(zmin = 0, zmax = 255):
+    zmid = (zmin + zmax) // 2
+    def hat(z):
+        return z - zmin if z <= zmin else zmax - z
+    return np.array([hat(z) for z in range(zmin, zmax + 1)], dtype = np.float)
+
+def get_z(images, pixel_positions):
+    ''' Images should be a list of 1-channel (R / G / B) images. '''
+    h, w = images[0].shape
+    z = np.zeros((len(pixel_positions), len(images)), dtype = np.float)
+    for i, (x, y) in enumerate(pixel_positions):
+        for j, img in enumerate(images):
+            z[i, j] = img[x, y]
+    return z
+
+
+images, exposures = [], []
+for f in image_files:
+    image, exposure = read_image(f)
+    images.append(image)
+    exposures.append(exposure)
+
+image_height, image_width, _ = images[0].shape
+
 pixel_positions = sample_pixels(image_height, image_width)
 
-    
+l = 1
+w_z = z_weights()
+b = np.array(exposures, dtype = np.float)
+z = get_z([img[:,:,0] for img in images], pixel_positions)
+print(z.shape)
